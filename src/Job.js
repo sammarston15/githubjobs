1e7fa0020fb78347e4624e1ec9decd48ae4b6091
Sam Marston <sammarston15@gmail.com>
2020-07-27 10:18:16 -0600

    added markdown dependency

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Badge } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 
 const Job = ({ job }) => {
   return (
@@ -8,15 +9,16 @@ const Job = ({ job }) => {
         <div className="d-flex justify-content-between">
           <div>
             <Card.Title>
-              {job.title} - <span className="text-muted font-weight-light">
-                {job.company}
-              </span>
+              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
             </Card.Title>
             <Card.Subtitle className='text-muted mb-2'>
               {new Date(job.created_at).toLocaleDateString()}
             </Card.Subtitle>
             <Badge variant='secondary' className='mr-2'>{job.type}</Badge>
             <Badge variant='secondary'>{job.location}</Badge>
+            <div>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
           </div>
         </div>
       </Card.Body>
